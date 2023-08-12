b2046a1438602f841ab90d0a89886abe58403665
Gwak-daeun <bbibibbibi22@gmail.com>
2023-08-12 14:02:56 +0900

    가위바위보 시작: 컴포넌트 틀 완성

---
+++ 가위바위보/RockScissorPaper.vue
@@ -0,0 +1,23 @@
+<template>
+    
+</template>
+
+<script>
+
+    export default {
+        data() {
+            
+        },
+        computed: {
+           
+        },
+        methods: {
+           
+        }
+    }; // Vue.component 부분 
+</script>
+
+<style scoped>
+/* scoped : css를 공유하지 않고 해당 컴포넌트에서만 쓰고 싶을 때  */
+
+</style>
---
+++ 가위바위보/main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'; // package.json에 설치한 vue를 가져옴
+
+import RockScissorPaper from './RockScissorPaper.vue';
+
+new Vue(RockScissorPaper).$mount('#root'); // 뷰 인스턴스
